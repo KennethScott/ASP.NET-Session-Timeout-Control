f10f8138e48a9eae2b440c71a4043c560dff6496
Kenneth Scott <mr.kenneth.scott@gmail.com>
2011-10-05 22:26:47 -0500

    v1.5 - added jQuery EasyUI demo

---
+++ Demo/EasyUI.Master.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Demo
+{
+    public partial class EasyUI : System.Web.UI.MasterPage
+    {
+        public bool TimeoutControlEnabled
+        {
+            get { return Timeout1.Enabled; }
+            set { Timeout1.Enabled = value; }
+        }
+
+        // can be used to access the clientid from content pages
+        public string TimeoutControlClientId
+        {
+            get { return Timeout1.ClientID; }
+        }
+    }
+}
---
+++ Demo/EasyUI.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/EasyUI.Master" AutoEventWireup="true" CodeBehind="EasyUI.aspx.cs" Inherits="Demo.EasyUI1" %>
+<%@ MasterType VirtualPath="~/EasyUI.Master" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ph1" runat="server">
+
+    <br />
+    <h2>
+        Welcome to the Timeout Demo app!
+    </h2>
+    <p>
+        To learn more about the Timeout control, visit <a href="http://programmerramblings.blogspot.com" title="Programmer Ramblings">Programmer Ramblings</a>.
+    </p>  
+    
+</asp:Content>
